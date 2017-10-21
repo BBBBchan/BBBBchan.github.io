50b6857aaab236f4abc43edf3c4f289bc1dcfd8f
BBBBchan <sbysbysby123@gmail.com>
2017-10-21 13:34:23 +0800

    1

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
-<?php
-	$a = -100;
-	$b = 50;
-	$c = 30;
+<html>
+<head>
+	<title>PHP测试</title>
+<head>
+	<body>	
+		<?php
+			$a = -100;
+			$b = 50;
+			$c = 30;
 
-	echo "\$a = ".$a.",";
-	echo "\$b = ".$b.",";
-	echo "\$c = ".$c."<p>";
-	echo "\$a + \$b =".($a + $b)."<br>";
-	echo "\$a - \$b =".($a - $b)."<br>";
-	echo "\$a * \$b =".($a * $b)."<br>";
-	echo "\$a / \$b =".($a / $b)."<br>";
-	echo "\$a / \$c =".($a / $c)."<br>";
-	
+			echo "\$a = ".$a.",";
+			echo "\$b = ".$b.",";
+			echo "\$c = ".$c."<p>";
+			echo "\$a + \$b =".($a + $b)."<br>";
+			echo "\$a - \$b =".($a - $b)."<br>";
+			echo "\$a * \$b =".($a * $b)."<br>";
+			echo "\$a / \$b =".($a / $b)."<br>";
+		echo "\$a / \$c =".($a / $c)."<br>";
+		?>
+	</body>
+</html>
